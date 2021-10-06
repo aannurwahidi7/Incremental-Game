2971a31e4de4511a492a3c7a97c0bf2e9e429554
Aan Nur Wahidi <aannurwahidi117@gmail.com>
2021-10-06 20:58:46 +0700

    add Incremental Game Firebase

---
+++ Assets/Scripts/TapArea.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TapArea : MonoBehaviour, IPointerDownHandler
+{
+    public AudioSource coinSounds;
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        GameManager.Instance.CollectByTap(eventData.position, transform);
+        coinSounds.Play();
+    }
+}
